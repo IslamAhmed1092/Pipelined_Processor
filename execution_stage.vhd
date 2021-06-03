69eec8941f2ed5d933aa10c971c17979e09c2ae9
IslamAhmed1092 <eslam1092@hotmail.com>
2021-06-03 23:46:57 +0200

    editing forwarding logic and testing memory instructions

--- execution_stage.vhd
+++ execution_stage.vhd
@@ -8,8 +8,8 @@ ENTITY execution_stage IS
 		ALU_Operation: IN std_logic_vector(4 downto 0);
 		SrcAndImm_in, HasImmediate_in: IN std_logic;
 		RsrcData_in, RdstData_in, Immediate_in: IN std_logic_vector(31 downto 0);
-		MEMdstA, WBdstA: IN std_logic;
-		MEMdstB, WBdstB: IN std_logic;
+		MEMdst, WBdst: IN std_logic;
+		MEMsrc, WBsrc: IN std_logic;
 		MEMWBData, WBWBData: IN std_logic_vector(31 downto 0);
 
 		ALU_Result: OUT std_logic_vector(31 downto 0);
@@ -40,29 +40,31 @@ END component;
 signal ALU_CCR_OUT: std_logic_vector(2 downto 0);
 signal CCR_OUT: std_logic_vector(2 downto 0);
 
-signal first_operand, second_operand: std_logic_vector(31 downto 0);
 signal A, B: std_logic_vector(31 downto 0);
+
+signal RdstData_Forwarded: std_logic_vector(31 downto 0);
+signal RsrcData_Forwarded: std_logic_vector(31 downto 0);
 
 BEGIN
 	alu_com : ALU port map (A, B, CCR_OUT, ALU_Operation, ALU_CCR_OUT, ALU_Result);
 	CCR: reg GENERIC MAP (3) port map(clk, reset, '1', '1', ALU_CCR_OUT, CCR_OUT);
 	
-	first_operand <= RdstData_in when SrcAndImm_in = '0'
-	else RsrcData_in;
 
-	second_operand <= RsrcData_in when HasImmediate_in = '0'
+	RdstData_Forwarded <= RdstData_in when MEMdst = '0' and WBdst = '0'
+	else WBWBData when MEMdst = '0' and WBdst = '1'
+	else MEMWBData when MEMdst = '1';
+
+	RsrcData_Forwarded <= RsrcData_in when MEMsrc = '0' and WBsrc = '0'
+	else WBWBData when MEMsrc = '0' and WBsrc = '1'
+	else MEMWBData when MEMsrc = '1';
+
+
+	A <= RdstData_Forwarded when SrcAndImm_in = '0'
+	else RsrcData_Forwarded;
+
+	B <= RsrcData_Forwarded when HasImmediate_in = '0'
 	else Immediate_in;
 
-	A <= first_operand when MEMdstA = '0' and WBdstA = '0'
-	else WBWBData when MEMdstA = '0' and WBdstA = '1'
-	else MEMWBData when MEMdstA = '1';
-
-	B <= second_operand when MEMdstB = '0' and WBdstB = '0'
-	else WBWBData when MEMdstB = '0' and WBdstB = '1'
-	else MEMWBData when MEMdstB = '1';
-
-	RdstData_out <= RdstData_in when MEMdstA = '0' and WBdstA = '0'
-	else WBWBData when MEMdstA = '0' and WBdstA = '1'
-	else MEMWBData when MEMdstA = '1';
+	RdstData_out <= RdstData_Forwarded;
 
 END execution_stage_Arch;
